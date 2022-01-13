b42f42ccf4d30a81cbdfaf156300f3a982ef85a8
CameronMackenzie99 <mackencameron@gmail.com>
2022-01-13 14:43:09 +0000

    Change opening of file to use "with" in export.py
    For resource allocation purposes and to satisfy the linter.

--- src/export.py
+++ src/export.py
@@ -12,6 +12,7 @@ def export_result(result, export_dir: str) -> None:
     timestamp = now.strftime("%Y-%m-%d,%H-%M-%S")
     with working_directory(export_dir):
         export_name = f"result_{timestamp}.json"
-        file = open(export_name, "w", encoding="utf-8")
-        encoded_result = jsonpickle.encode(result, unpicklable=False, indent=2, use_decimal=True)
-        file.write(encoded_result) # type: ignore
+        with open(export_name, "w", encoding="utf-8") as file:
+            encoded_result = jsonpickle.encode(result, unpicklable=False,
+                            indent=2, use_decimal=True)
+            file.write(encoded_result) # type: ignore
